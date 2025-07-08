2054d185acadef4165c13669d4b6689717e3beeb
s-marsi <soufianemarsi1999@gmail.com>
2025-07-08 15:05:55 +0100

    feat: define check_valide_path methode

--- Includes/HttpRequest.hpp
+++ Includes/HttpRequest.hpp
@@ -10,42 +10,56 @@ struct S_Header {
 };
 
 enum ParseResult {
-    OK,
-    BadRequest,         // 400 - Malformed request syntax or invalid headers
-    NotAllowed,         // 405 - Method not allowed
-    Incomplete,         // Request is not complete yet (waiting for more data)
-    URITooLong,         // 414 - URI too long
-    PayloadTooLarge,    // 413 - Content-Length too big
-    HTTPVersionNotSupported, // 505 - HTTP version not supported
-    UnsupportedMediaType,    // 415 - Unsupported Content-Type
-    HeaderFieldsTooLarge,    // 431 - Too large header fields
-    InternalError       // 500 - Internal server error during parsing
+    OK,                         // 0 - Success
+    Incomplete,                 // 1 - Waiting for more data (not an error)
+    // Common 4xx client-side errors
+    BadRequest,                 // 2 - 400
+    NotAllowed,                 // 3 - 405
+    LengthRequired,             // 4 - 411
+    PayloadTooLarge,            // 5 - 413
+    URITooLong,                 // 6 - 414
+    UnsupportedMediaType,       // 7 - 415
+    HeaderFieldsTooLarge,       // 8 - 431
+    NotFound,                   // 9 - 404
+    Forbidden,                  //10 - 403
+    Gone,                       //11 - 410
+    Conflict,                   //12 - 409
+    RequestTimeout,             //13 - 408
+
+    // Server-side errors
+    HTTPVersionNotSupported,    //14 - 505
+    InternalError               //15 - 500
 };
+
 
 class HttpRequest
 {
     std::string             r_method, r_url, r_version, r_query;
     std::vector<S_Header>   r_header;
     std::string             r_body;
+    std::string             r_host;
+    std::string             r_content_type;
     std::string             path;
     unsigned int            r_content_length;
     bool                    r_has_content_length;
     bool                    r_has_transfer_encoding;
-    std::string             r_host;
     ServerConfig            server ;
     LocationConfig          location;
     public:
-        HttpRequest();
-        ParseResult parse(std::string request);
-        ParseResult parse_start_line(std::string &start_line);
-        ParseResult parse_header(std::string &header);
-        ParseResult parse_body();
-        ~HttpRequest();
+        HttpRequest( );
+        ParseResult parse( std::string request );
+        ParseResult parse_start_line( std::string &start_line );
+        ParseResult parse_header( std::string &header );
+        ParseResult parse_body( );
+        ~HttpRequest( );
 
-        ParseResult     setServer();
-        ParseResult     setLocation();
-        ServerConfig&   getServer() ;
-        LocationConfig& getLocation() ;
+        ParseResult     setServer( );
+        ParseResult     setLocation( );
+        ServerConfig&   getServer( ) ;
+        LocationConfig& getLocation( ) ;
+        void            setPath( std::string &root, std::string &url ) ;
+        std::string &   getPath( ) ;
+        ParseResult     check_valid_path( ) ;
 };
 
 bool        find_and_get(std::string &request, std::string &dest, std::string delimeter) ;
