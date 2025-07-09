33ce8ab18b7b196653eb9410aea2e00cecc458dc
s-marsi <soufianemarsi1999@gmail.com>
2025-07-09 15:11:33 +0100

    fix: update parsing logic to work while reading request from client

--- Includes/HttpRequest.hpp
+++ Includes/HttpRequest.hpp
@@ -10,30 +10,34 @@ struct S_Header {
 };
 
 enum ParseResult {
-    OK,                         // 0 - Success
+    OK = 200,                         // 0 - Success
     Incomplete,                 // 1 - Waiting for more data (not an error)
     // Common 4xx client-side errors
-    BadRequest,                 // 2 - 400
-    NotAllowed,                 // 3 - 405
-    LengthRequired,             // 4 - 411
-    PayloadTooLarge,            // 5 - 413
-    URITooLong,                 // 6 - 414
-    UnsupportedMediaType,       // 7 - 415
-    HeaderFieldsTooLarge,       // 8 - 431
-    NotFound,                   // 9 - 404
-    Forbidden,                  //10 - 403
-    Gone,                       //11 - 410
-    Conflict,                   //12 - 409
-    RequestTimeout,             //13 - 408
+    BadRequest = 400,                 // 2 - 400
+    NotAllowed = 405,                 // 3 - 405
+    LengthRequired = 411,             // 4 - 411
+    PayloadTooLarge = 413,            // 5 - 413
+    URITooLong = 414,                 // 6 - 414
+    UnsupportedMediaType = 415,       // 7 - 415
+    HeaderFieldsTooLarge = 431,       // 8 - 431
+    NotFound = 404,                   // 9 - 404
+    Forbidden = 403,                  //10 - 403
+    Gone = 410,                       //11 - 410
+    Conflict = 409,                   //12 - 409
+    RequestTimeout = 408,             //13 - 408
 
     // Server-side errors
-    HTTPVersionNotSupported,    //14 - 505
-    InternalError               //15 - 500
+    InternalError = 500 ,             //14 - 500
+    NotImplemented = 501,             //15 - 501
+    HTTPVersionNotSupported = 505    //16 - 505
 };
 
+enum E_STATUS { Start_Line, Header, Body, END } ; 
 
 class HttpRequest
 {
+    std::string             r_buffer;
+    size_t                  r_current_body_size;
     std::string             r_method, r_url, r_version, r_query;
     std::vector<S_Header>   r_header;
     std::string             r_body;
@@ -43,6 +47,8 @@ class HttpRequest
     unsigned int            r_content_length;
     bool                    r_has_content_length;
     bool                    r_has_transfer_encoding;
+    ParseResult             r_status_code;
+    E_STATUS                r_read_status;
     ServerConfig            server ;
     LocationConfig          location;
     public:
@@ -51,6 +57,8 @@ class HttpRequest
         ParseResult parse_start_line( std::string &start_line );
         ParseResult parse_header( std::string &header );
         ParseResult parse_body( );
+        ParseResult parse_chunked_body( std::string &tmp );
+        ParseResult parse_trailer_header( std::string &body );
         ~HttpRequest( );
 
         ParseResult     setServer( );
@@ -59,6 +67,11 @@ class HttpRequest
         LocationConfig& getLocation( ) ;
         void            setPath( std::string &root, std::string &url ) ;
         std::string &   getPath( ) ;
+        E_STATUS &      getReadStatus( ) ;
+        void            setReadStatus( E_STATUS status ) ;
+        size_t &        getCurrentBodySize( ) ;
+        void            setCurrentBodySize( size_t size ) ;
+        std::string &   getBody( ) ;
         ParseResult     check_valid_path( ) ;
 };
 
