4e869eebe9d0faee137517145d52be8ee0c978c8
s-marsi <soufianemarsi1999@gmail.com>
2025-07-13 19:12:46 +0100

    feat: handle GET errors

--- src/HttpResponse/GET/get_handler.cpp
+++ src/HttpResponse/GET/get_handler.cpp
@@ -6,7 +6,7 @@ bool ends_with(const std::string &str, const std::string &suffix) {
 }
 
 
-std::string getResponseType(const std::string& path) {
+static std::string getResponseType( const std::string& path ) {
     if (ends_with(path, ".html")) return "text/html";
     if (ends_with(path, ".css")) return "text/css";
     if (ends_with(path, ".js")) return "application/javascript";
@@ -20,30 +20,35 @@ void    HttpResponse::handle_get(HttpRequest& request, std::string &response)
 {
 
     ParseResult ServerResult = find_server_location( &request );
-    ParseResult Pathresult = request.check_valid_path( );
-
+    
     if ( ServerResult != OK)
     {
-        response = HttpResponse::create_response( ServerResult, to_string( ServerResult ) );
-    }
-    else if ( Pathresult != OK)
-    {        
-        response = HttpResponse::create_response( Pathresult, to_string( Pathresult ) );
+        Errors errors;
+        response = errors.handle_error( request.getServer().error_pages, ServerResult ) ;
     }
     else
     {
-        std::ifstream file(request.getPath().c_str(), std::ios::binary);
-        std::ostringstream contentStream;
-        contentStream << file.rdbuf();
-        std::string body = contentStream.str();
+        ParseResult Pathresult = request.check_valid_path( );
+        if ( Pathresult != OK)
+        {   
+            Errors errors;
+            response = errors.handle_error( request.getServer().error_pages, Pathresult ) ;
+        }
+        else
+        {
+            std::ifstream file(request.getPath().c_str(), std::ios::binary);
+            std::ostringstream contentStream;
+            contentStream << file.rdbuf();
+            std::string body = contentStream.str();
 
 
-        response = "HTTP/1.1 200 OK\r\n";
-        response += "Content-Type: " + getResponseType(request.getPath()) + "\r\n";
-        size_t len = body.size();
-        response += "Content-Length: " + to_string(len) + "\r\n";
-        response += "Connection: close\r\n";
-        response += "\r\n";
-        response += body;
-    }
+            response = "HTTP/1.1 200 OK\r\n";
+            response += "Content-Type: " + getResponseType(request.getPath()) + "\r\n";
+            size_t len = body.size();
+            response += "Content-Length: " + to_string(len) + "\r\n";
+            response += "Connection: close\r\n";
+            response += "\r\n";
+            response += body;
+        }
+    }    
 }
