176e54c1d95ec6ec3f5ff393d780a48b0e6a1d3a
s-marsi <soufianemarsi1999@gmail.com>
2025-07-22 19:07:52 +0100

    chore: move matching server code to matching.cpp

--- src/HttpResponse/GET/get_handler.cpp
+++ src/HttpResponse/GET/get_handler.cpp
@@ -44,50 +44,35 @@ void    handle_redirection(HttpRequest& request, std::string &response)
 
 void    HttpResponse::handle_get(HttpRequest& request, std::string &response)
 {
-
-    ParseResult ServerResult = find_server_location( &request );
-    
-    if ( ServerResult != OK)
+    if ( !request.getLocation().redirection_code.empty() )
     {
+        handle_redirection( request, response ) ;
+        return ;
+    }
+    ParseResult Pathresult = request.check_valid_path( );
+    if ( Pathresult != OK)
+    {   
         Errors errors;
-        response = errors.handle_error( request.getServer().error_pages, ServerResult ) ;
+        response = errors.handle_error( request.getServer().error_pages, Pathresult ) ;
+        return ;
+    }
+    std::string body = "";
+    if ( request.getAutoIndex() == true )
+    {
+        handle_auto_index_response( request, body );
     }
     else
     {
-        if ( !request.getLocation().redirection_code.empty() )
-        {
-            handle_redirection( request, response ) ;
-            return ;
-        }
-        ParseResult Pathresult = request.check_valid_path( );
-        if ( Pathresult != OK)
-        {   
-            Errors errors;
-            response = errors.handle_error( request.getServer().error_pages, Pathresult ) ;
-        }
-        else
-        {
-            std::string body = "";
-            if ( request.getAutoIndex() == true )
-            {
-
-                handle_auto_index_response( request, body );
-            }
-            else
-            {
-                std::ifstream file(request.getPath().c_str(), std::ios::binary);
-                std::ostringstream contentStream;
-                contentStream << file.rdbuf();
-                body = contentStream.str();
-            }
-            response = "HTTP/1.1 200 OK\r\n";
-            response += "Content-Type: " + getResponseType(request.getPath()) + "\r\n";
-            size_t len = body.size();
-            response += "Content-Length: " + to_string(len) + "\r\n";
-            response += "Connection: close\r\n";
-            response += "\r\n";
-            response += body; 
-
-        }
-    }    
+        std::ifstream file(request.getPath().c_str(), std::ios::binary);
+        std::ostringstream contentStream;
+        contentStream << file.rdbuf();
+        body = contentStream.str();
+    }
+    response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-Type: " + getResponseType(request.getPath()) + "\r\n";
+    size_t len = body.size();
+    response += "Content-Length: " + to_string(len) + "\r\n";
+    response += "Connection: close\r\n";
+    response += "\r\n";
+    response += body; 
 }
