28ad16ffadc3ed5c3eac67f24d0d362978e88ad2
s-marsi <soufianemarsi1999@gmail.com>
2025-08-22 18:46:55 +0100

    fix: improve header validation for empty header

--- src/HttpRequest/parseRequest/parseRequest.cpp
+++ src/HttpRequest/parseRequest/parseRequest.cpp
@@ -11,7 +11,7 @@ ParseResult HttpRequest::parse( std::string buffer )
         if ( !find_and_get(r_buffer, start_line, "\r\n") )
         {
             size_t index = r_buffer.find("\r");
-            if (index != std::string::npos && r_buffer.length() - 1 > index)
+            if ( index != std::string::npos && r_buffer.length() - 1 > index )
                 throw ( BadRequest );
             throw ( Incomplete );
         }
@@ -25,7 +25,11 @@ ParseResult HttpRequest::parse( std::string buffer )
     if ( r_read_status == Header )
     {
         if ( !find_and_get(r_buffer, header, "\r\n\r\n") )
+        {
+            if ( r_buffer.empty() || r_buffer == "\r\n")
+                throw ( BadRequest );
             throw ( Incomplete );  
+        }
 
         result = parse_header(header);
 
