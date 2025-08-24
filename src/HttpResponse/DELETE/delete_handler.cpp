4228da0cdeff81874165331a23aa0ce38b493024
s-marsi <soufianemarsi1999@gmail.com>
2025-08-24 11:13:58 +0100

    refactor: move file deletion logic to a separate function

--- src/HttpResponse/DELETE/delete_handler.cpp
+++ src/HttpResponse/DELETE/delete_handler.cpp
@@ -1,17 +1,50 @@
 #include "HttpResponse.hpp"
 
+void return_error(HttpRequest& request, std::string &response, ParseResult result)
+{
+    Errors errors;
+    response = errors.handle_error( request.getServer().error_pages, result ) ;  
+}
+
+void remove_file_directory(HttpRequest& request, std::string &response, std::string &path)
+{
+    std::string dirPath = request.getLocation().root;
+    ParseResult Pathresult = isDirectoryAndAccessible( dirPath );
+    if ( Pathresult != OK)
+        return_error(request, response, Pathresult);
+    else
+    {
+        std::string body = "";
+        std::remove( path.c_str() );
+        ParseResult Pathresult = isFileAndAccessible( request.getPath(), R_OK );
+        if (Pathresult == NotFound)
+        {
+            body = "DELETE: file deleted successfully!\n";
+            Pathresult = OK;
+        }
+        else
+        {
+            body = "DELETE: cannot delete the file.!";
+
+            if (Pathresult == OK)
+            Pathresult = InternalError;
+        }
+        response = create_res( Pathresult, body, ".html");
+    }
+}
 
 void    HttpResponse::handle_delete(HttpRequest& request, std::string &response)
 {
     std::string path = request.getPath();
+
+    
     if ( !path.empty() && path[path.length() - 1] == '/' )
         path.erase( path.size() - 1 );
 
     ParseResult Pathresult = isDirectoryAndAccessible( path );
     if ( Pathresult != NotFound)
     {
-        Errors errors;
-        response = errors.handle_error( request.getServer().error_pages, Forbidden ) ;
+        return_error(request, response, Forbidden);
         return ;
     }
 
@@ -19,37 +52,9 @@ void    HttpResponse::handle_delete(HttpRequest& request, std::string &response)
 
     if ( Pathresult != OK)
     {
-        Errors errors;
-        response = errors.handle_error( request.getServer().error_pages, Pathresult ) ;
+        return_error(request, response, Pathresult);
         return ;
     }
     else
-    {
-        std::string dirPath = request.getLocation().root;
-        ParseResult Pathresult = isDirectoryAndAccessible( dirPath );
-        if ( Pathresult != OK)
-        {
-            Errors errors;
-            response = errors.handle_error( request.getServer().error_pages, Pathresult ) ;
-        }
-        else
-        {
-            std::string body = "";
-            std::remove( path.c_str() );
-            ParseResult Pathresult = isFileAndAccessible( request.getPath(), R_OK );
-            if (Pathresult == NotFound)
-            {
-                body = "DELETE: file deleted successfully!\n";
-                Pathresult = OK;
-            }
-            else
-            {
-                body = "DELETE: cannot delete the file.!";
-
-                if (Pathresult == OK)
-                Pathresult = InternalError;
-            }
-            response = create_res( Pathresult, body, ".html");
-        }
-    }
+        remove_file_directory(request, response, path);
 }
