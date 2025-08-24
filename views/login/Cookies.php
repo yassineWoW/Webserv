e0b8011631e37dc9320cad7cb4629200ef7a93cd
s-marsi <soufianemarsi1999@gmail.com>
2025-08-24 10:38:00 +0100

    feat(cookies): add color selection and store it in cookies

--- views/login/Cookies.php
+++ views/login/Cookies.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     $username = isset($_COOKIE["user"]) ? $_COOKIE["user"] : (isset($_POST["username"]) ? $_POST["username"] : "default");
+    $color = isset($_COOKIE["color"]) ? $_COOKIE["color"] : (isset($_POST["color"]) ? $_POST["color"] : "black");
+
 ?>
 
 <html lang="en">
@@ -8,9 +10,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookies</title>
+    <style>
+        h2 {
+            color: <?php echo $color ?>;
+        }
+    </style>
 </head>
 <body>
     <div class="print_name">
-        <h2>Hello <?php echo $username; ?></h2>
+        <h2>Hello <b> <?php echo $username; ?> </b></h2>
     </div>
 </html>
--- views/login/login.php
+++ views/login/login.php
@@ -3,6 +3,11 @@
     $cookie_name = "user";
     $cookie_value = !isset($_POST["username"]) ||  empty($_POST["username"]) ? "default" : $_POST["username"];
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+    if (isset($_POST["color"]) && !empty($_POST["color"])) {
+        $color_name = "color";
+        $color_value = $_POST["color"];
+        setcookie($color_name, $color_value, time() + (86400 * 30), "/");
+    }
     header("Location: /login/Cookies.php");
     exit;
 ?>
